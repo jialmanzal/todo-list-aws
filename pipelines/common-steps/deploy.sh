76c0fae2ebc0d498d23f8f3498687185a86ff9f6
jialmanzal <jesusalmanza@gmail.com>
2022-06-12 13:34:00 -0500

    Update deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,5 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name ${ENVIRONMENT}
+echo ${ENVIRONMENT}
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name=${ENVIRONMENT}
